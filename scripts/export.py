fdd66e0653a1ed86f385fa531191346a260ac487
Redclem <clem77.remy@gmail.com>
2023-01-17 21:28:16 +0100

    Restructuration massive

---
+++ scripts/export.py
@@ -0,0 +1,78 @@
+from anki.storage import Collection
+from os import mkdir, getcwd
+from os.path import isdir, isfile
+from anki.collection import DeckIdLimit
+from hashlib import sha256
+
+class MyCol(Collection):
+    def __init__(self, path):
+        Collection.__init__(self, path)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+
+
+with MyCol("/home/redclem/.local/share/Anki2/Utilisateur 1/collection.anki2") as col:
+
+    hashes_c = {}
+    
+    for noteid in col.find_notes(""):
+        note = col.get_note(noteid)
+        if len(note.cards()) == 0: continue
+        did = note.cards()[0].current_deck_id()
+
+        if did not in hashes_c.keys():
+            hashes_c[did] = sha256()
+
+        for f in note.fields:
+            hashes_c[did].update(f.encode())
+
+    hashes_i = {}
+    if isfile("hashes.txt"):
+        with open("hashes.txt", "r") as file:
+            data = file.read()
+
+        for tok in data.split(";"):
+            if tok == "": continue
+            spl = tok.split(":")
+            hashes_i[int(spl[0])] = spl[1]
+    
+    decks_n_i = col.decks.all_names_and_ids()
+
+    def exists_or_create(path):
+        if not isdir(path):
+            mkdir(path)
+
+    for deck in decks_n_i:
+        did = deck.id
+        dname = deck.name
+
+        tokens = dname.split("::")
+        filename = tokens.pop()
+
+        path = getcwd()
+        exists_or_create(path)
+
+        for i in tokens:
+            path += '/'
+            path += i
+            exists_or_create(path)
+
+        path += '/' + filename + ".apkg"
+
+        lim = DeckIdLimit(did)
+
+        if (did in hashes_i.keys() and hashes_i[did] == hashes_c[did].hexdigest()) or not did in hashes_c.keys():
+            print("Skipping {}".format(filename))
+        else:
+            print("Exporting {}".format(filename))
+            col.export_anki_package(out_path=path, limit=lim, with_scheduling=False, with_media=False, legacy_support=False)
+
+
+    with open("hashes.txt", "w") as f:
+        for k,v in hashes_c.items():
+            f.write(str(k) + ':' + v.hexdigest() + ';')
